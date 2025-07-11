ff51c9851961cab4cbfb78033d0100b894cf92e6
RayVenShellhart <rayvenshellhart@gmail.com>
2025-07-11 13:26:29 -0700

    rebuild-code

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -240,3 +240,11 @@ VALUES   (
     'White',
     5
   );
+
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interiors')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, 'images/', 'images/vehicles/'), inv_thumbnail = REPLACE(inv_thumbnail, 'images/', 'images/vehicles/');
+
